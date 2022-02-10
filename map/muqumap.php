f01ae35450f48e3686ebca5c345507813cf639fb
RahafMAlharbi <rhfproject221@gmail.com>
2022-02-10 09:27:45 +0300

    table

--- map/muqumap.php
+++ map/muqumap.php
@@ -25,67 +25,69 @@
       </head>
       <body>
           <div id="map"></div>
-              <div class="container ">
-                  <h1>Click the filter icon <small>(<i class="glyphicon glyphicon-filter"></i>)</small></h1>
-                  	<div class="row">
-              			<div class="col-md-6">
-              				<div class="panel panel-primary table-wrapper-scroll-y my-custom-scrollbar">
-              					<div class="panel-heading">
-              						<h3 class="panel-title">Developers</h3>
-              						<div class="pull-right">
-              							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
-              								<i class="glyphicon glyphicon-filter"></i>
-              							</span>
-              						</div>
-              					</div>
-              					<div class="panel-body">
-              						<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
-              					</div>
-              					<table class="table table-hover table table-bordered table-striped mb-0" id="dev-table">
-              						<thead>
-              							<tr>
-              								<th>ID</th>
-              								<th>Date</th>
-              								<th>Category</th>
-              								<th>Status</th>
-              							</tr>
-              						</thead>
-              						<tbody>
-                            <?php
-                            $servername = "localhost";
-                            $username = "root";
-                            $password = "";
-                            $dbname = "muqu";
-                            $conn = new mysqli($servername, $username, $password, $dbname);
-                            if ($conn->connect_error) {
-                              die("Connection failed: " . $conn->connect_error);
-                            }
+
+                  <div class="container ">
+                      <!-- <h1><small>(<i class="glyphicon glyphicon-filter"></i>)</small></h1> -->
+                      	<div class="row">
+                  			<div class="col-md-6">
+                  				<div class="panel panel-primary table-wrapper-scroll-y my-custom-scrollbar">
+                  					<div class="panel-heading">
+                  						<h3 class="panel-title">Complaint</h3>
+                  						<div class="pull-right">
+                  							<span class="clickable filter" data-toggle="tooltip" title="Toggle table filter" data-container="body">
+                  								<i class="glyphicon glyphicon-filter"></i>
+                  							</span>
+                  						</div>
+                  					</div>
+                  					<div class="panel-body">
+                  						<input type="text" class="form-control" id="dev-table-filter" data-action="filter" data-filters="#dev-table" placeholder="Filter Developers" />
+                  					</div>
+                  					<table class="table table-hover table table-bordered table-striped mb-0" id="dev-table">
+                  						<thead>
+                  							<tr>
+                  								<th>ID</th>
+                  								<th>Date</th>
+                  								<th>Category</th>
+                  								<th>Status</th>
+                  							</tr>
+                  						</thead>
+                  						<tbody>
+                                <?php
+                                $servername = "localhost";
+                                $username = "root";
+                                $password = "";
+                                $dbname = "muqu";
+                                $conn = new mysqli($servername, $username, $password, $dbname);
+                                if ($conn->connect_error) {
+                                  die("Connection failed: " . $conn->connect_error);
+                                }
 
 
-                            ?>
+                                ?>
 
-                            <?php
-                            $qury = mysqli_query($conn,"select complaintId ,date,category,status,description,roomNum from complaint");
-                             $romNumerquery = mysqli_query($conn,"select roomNumber from classroom");
-                             //$result = $conn->query($sql);
-                             if ($qury-> num_rows >0){
-                               while ($row = $qury-> fetch_assoc()) {
-                                 echo "<tr><td>".$row["complaintId"]."</td>"."<td>".$row["date"]."</td>"."<td>".$row["category"]."</td>"."<td>".$row["status"]."</td></tr>";
-                               }
-                               echo "</tbody>";
+                                <?php
+                                $qury = mysqli_query($conn,"select complaintId ,date,category,status,description,roomNum from complaint");
+                                 $romNumerquery = mysqli_query($conn,"select roomNumber from classroom");
+                                 //$result = $conn->query($sql);
+                                 if ($qury-> num_rows >0){
+                                   while ($row = $qury-> fetch_assoc()) {
+                                     echo "<tr><td>".$row["complaintId"]."</td>"."<td>".$row["date"]."</td>"."<td>".$row["category"]."</td>"."<td>".$row["status"]."</td></tr>";
+                                   }
+                                   echo "</tbody>";
 
-                             }
-                             else {
+                                 }
+                                 else {
 
-                               echo "no result";
-                             }
-                             $conn->close();
-                             ?>
+                                   echo "no result";
+                                 }
+                                 $conn->close();
+                                 ?>
 
-              						</tbody>
-              					</table>
-              				</div>
-              			</div>
+                  						</tbody>
+                  					</table>
+                  				</div>
+                  			</div>
+
 
 <style >
   #map { 		height: 400px;
@@ -97,7 +99,16 @@
     height: 400px;
 }
 
-/*popup taple css */
+/*taple css */
+
+.my-custom-scrollbar {
+position: relative;
+height: 250px;
+overflow: auto;
+}
+.table-wrapper-scroll-y {
+display: block;
+}
 
 table{
     border:1px solid black;
@@ -116,6 +127,56 @@ td{
 
 
 <script >
+// table js
+(function(){
+  'use strict';
+var $ = jQuery;
+$.fn.extend({
+  filterTable: function(){
+    return this.each(function(){
+      $(this).on('keyup', function(e){
+        $('.filterTable_no_results').remove();
+        var $this = $(this),
+                      search = $this.val().toLowerCase(),
+                      target = $this.attr('data-filters'),
+                      $target = $(target),
+                      $rows = $target.find('tbody tr');
+
+        if(search == '') {
+          $rows.show();
+        } else {
+          $rows.each(function(){
+            var $this = $(this);
+            $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+          })
+          if($target.find('tbody tr:visible').size() === 0) {
+            var col_count = $target.find('tr').first().find('td').size();
+            var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
+            $target.find('tbody').append(no_results);
+          }
+        }
+      });
+    });
+  }
+});
+$('[data-action="filter"]').filterTable();
+})(jQuery);
+
+$(function(){
+  // attach table filter plugin to inputs
+$('[data-action="filter"]').filterTable();
+
+$('.container').on('click', '.panel-heading span.filter', function(e){
+  var $this = $(this),
+    $panel = $this.parents('.panel');
+
+  $panel.find('.panel-body').slideToggle();
+  if($this.css('display') != 'none') {
+    $panel.find('.panel-body input').focus();
+  }
+});
+$('[data-toggle="tooltip"]').tooltip();
+})
 //popup table
 var template =
 '<div class="panel-body">\
@@ -188,55 +249,105 @@ var popup =singleMarker.bindPopup(template);
 popup.addTo(map);
 //////////////
 
-(function(){
-  'use strict';
-var $ = jQuery;
-$.fn.extend({
-  filterTable: function(){
-    return this.each(function(){
-      $(this).on('keyup', function(e){
-        $('.filterTable_no_results').remove();
-        var $this = $(this),
-                      search = $this.val().toLowerCase(),
-                      target = $this.attr('data-filters'),
-                      $target = $(target),
-                      $rows = $target.find('tbody tr');
-
-        if(search == '') {
-          $rows.show();
-        } else {
-          $rows.each(function(){
-            var $this = $(this);
-            $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
-          })
-          if($target.find('tbody tr:visible').size() === 0) {
-            var col_count = $target.find('tr').first().find('td').size();
-            var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
-            $target.find('tbody').append(no_results);
-          }
-        }
-      });
-    });
-  }
-});
-$('[data-action="filter"]').filterTable();
-})(jQuery);
-
-$(function(){
-  // attach table filter plugin to inputs
-$('[data-action="filter"]').filterTable();
-
-$('.container').on('click', '.panel-heading span.filter', function(e){
-  var $this = $(this),
-    $panel = $this.parents('.panel');
-
-  $panel.find('.panel-body').slideToggle();
-  if($this.css('display') != 'none') {
-    $panel.find('.panel-body input').focus();
-  }
-});
-$('[data-toggle="tooltip"]').tooltip();
-})
+// (function(){
+//     'use strict';
+// 	var $ = jQuery;
+// 	$.fn.extend({
+// 		filterTable: function(){
+// 			return this.each(function(){
+// 				$(this).on('keyup', function(e){
+// 					$('.filterTable_no_results').remove();
+// 					var $this = $(this),
+//                         search = $this.val().toLowerCase(),
+//                         target = $this.attr('data-filters'),
+//                         $target = $(target),
+//                         $rows = $target.find('tbody tr');
+//
+// 					if(search == '') {
+// 						$rows.show();
+// 					} else {
+// 						$rows.each(function(){
+// 							var $this = $(this);
+// 							$this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+// 						})
+// 						if($target.find('tbody tr:visible').size() === 0) {
+// 							var col_count = $target.find('tr').first().find('td').size();
+// 							var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
+// 							$target.find('tbody').append(no_results);
+// 						}
+// 					}
+// 				});
+// 			});
+// 		}
+// 	});
+// 	$('[data-action="filter"]').filterTable();
+// })(jQuery);
+//
+// $(function(){
+//     // attach table filter plugin to inputs
+// 	$('[data-action="filter"]').filterTable();
+//
+// 	$('.container').on('click', '.panel-heading span.filter', function(e){
+// 		var $this = $(this),
+// 			$panel = $this.parents('.panel');
+//
+// 		$panel.find('.panel-body').slideToggle();
+// 		if($this.css('display') != 'none') {
+// 			$panel.find('.panel-body input').focus();
+// 		}
+// 	});
+// 	$('[data-toggle="tooltip"]').tooltip();
+// })
+// (function(){
+//   'use strict';
+// var $ = jQuery;
+// $.fn.extend({
+//   filterTable: function(){
+//     return this.each(function(){
+//       $(this).on('keyup', function(e){
+//         $('.filterTable_no_results').remove();
+//         var $this = $(this),
+//                       search = $this.val().toLowerCase(),
+//                       target = $this.attr('data-filters'),
+//                       $target = $(target),
+//                       $rows = $target.find('tbody tr');
+//
+//         if(search == '') {
+//           $rows.show();
+//         } else {
+//           $rows.each(function(){
+//             var $this = $(this);
+//             $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+//           })
+//           if($target.find('tbody tr:visible').size() === 0) {
+//             var col_count = $target.find('tr').first().find('td').size();
+//             var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
+//             $target.find('tbody').append(no_results);
+//           }
+//         }
+//       });
+//     });
+//   }
+// });
+// $('[data-action="filter"]').filterTable();
+// })(jQuery);
+//
+// $(function(){
+//   // attach table filter plugin to inputs
+// $('[data-action="filter"]').filterTable();
+//
+// $('.container').on('click', '.panel-heading span.filter', function(e){
+//   var $this = $(this),
+//     $panel = $this.parents('.panel');
+//
+//   $panel.find('.panel-body').slideToggle();
+//   if($this.css('display') != 'none') {
+//     $panel.find('.panel-body input').focus();
+//   }
+// });
+// $('[data-toggle="tooltip"]').tooltip();
+// })
+// table filter
 
 </script>
 
@@ -264,15 +375,15 @@ if ($conn->connect_error) {
 ?>
 <form class="row g-3 needs-validation" novalidate>
 <div class="col-md-3">
-  <!-- <label for="validationCustom04" class="form-label">Room Number</label> -->
-  <!-- <select class="form-select" id="validationCustom04" required> -->
+  <label for="validationCustom04" class="form-label">Room Number</label>
+  <select class="form-select" id="validationCustom04" required>
     <?php
-      // for($i=1;$i<=$rowcount;$i++){
-      // $row=mysqli_fetch_array($romNumerquery);
+      for($i=1;$i<=$rowcount;$i++){
+      $row=mysqli_fetch_array($romNumerquery);
      ?>
-    <!-- <option value="<?php echo $row["roomNumber"]?>"> <?php echo $row["roomNumber"]?> </option> -->
+    <option value="<?php echo $row["roomNumber"]?>"> <?php echo $row["roomNumber"]?> </option>
 
 
     <?php
-//  }
+ }
      ?>
