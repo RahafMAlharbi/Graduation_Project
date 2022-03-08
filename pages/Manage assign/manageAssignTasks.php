ed718522e17ff1153f118e0901f2792549150232
RahafMAlharbi <97194765+RahafMAlharbi@users.noreply.github.com>
2022-03-08 18:11:58 +0300

    Update manageAssignTasks.php

--- pages/Manage assign/manageAssignTasks.php
+++ pages/Manage assign/manageAssignTasks.php
@@ -1,4 +1,8 @@
-
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
          <link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
          <script src="bootstrap5/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
          <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
@@ -20,9 +24,11 @@
            <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
 
            <script src="~/Scripts/jquery-3.4.1.js"></Script>
+           </head>
 
 
          <style>
@@ -35,6 +41,9 @@
 
          .height {
              height: 100vh
+         }
+         .modal-backdrop {
+           z-index: -1;
          }
          </style>
            <?php
@@ -73,7 +82,7 @@
          				<h2> Assign Emploess Complain</h2>
          			  </div>
          			  <div class="card-body">
-         				 <form  id="form" name ="myFormName" action ="setStatusSolved.php" method="POST" class="row g-3 needs-validation" novalidate>
+         				 <form  id="form" name ="myFormName" action ="anothercopyassign.php" method="POST" class="row g-3 needs-validation" novalidate>
          									<div class="col col-md-12">
          				<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for complaint id.." >
          				<br/>
@@ -102,7 +111,9 @@
          						// $result1 = $conn->query($sql1);
          						echo"<tr>";
          						echo"<td> <input type='checkbox'  class='box' name='box_".$row["complaintId"]."' id='box_".$row["complaintId"]."' onchange='callFunction();callFunction1()' onclick='enable(this);chk(this)' /></td>";
-                    //button for info window
+                    // echo'<td> <input type="checkbox"  class="box" name='.$row["complaintId"].' id='.$row["complaintId"].' onchange="callFunction();callFunction1()" onclick="enable(this);chk(this)" /></td>';
+
+                  //button for info window
                     // $val="'.$row["complaintId"].'";
                      echo'
                       <td name=name_"'.$row["complaintId"].'">
@@ -122,7 +133,7 @@
 
          						// echo"<td> <select class='form-select' id='SelectPicker_".$row["complaintId"]."' name='SelectPicker_".$row["complaintId"]."' disabled>";
          						// echo "<option value='0'   >select worker </option>";
-                   //
+
          						// while($row1=$result1->fetch_assoc()){
                    //
                    //
@@ -135,9 +146,10 @@
          				  </tbody>
          				  </table>
          					<input type="submit" value="Confirm" class="btn btn-primary" disabled id="Assign">
-                  <input type="button" value="Reassign" class="btn btn-primary" disabled id=""  data-target="#exampleModal" >
-                  <!-- <input type="submit" value="Assign" class="btn btn-primary" disabled id="Assign" onclick="validateMyForm(event);"> -->
+                  <input type="button" value="Reassign" class="btn btn-primary" disabled id=""  data-toggle="modal" disabled data-target="#exampleModal2" onclick="LoadA()">
+                  <!-- <input type="button"onclick="selectRoomNum2()"> -->
 
+                  <!-- <input type="button" value="Assign" class="btn btn-primary" disabled id="Assign" onclick="selectRoomNum2()"> -->
 
                 </div>
 
@@ -147,9 +159,33 @@
          			</div>
          		</div>
          	</div>
+<!-- reassign window -->
+                    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header float-right">
+                                    <h5>User details</h5>
+                                    <div class="text-right"> <i data-dismiss="modal" aria-label="Close" class="fa fa-close"></i> </div>
+                                </div>
+                                <div class="modal-body">
+                                    <div>
+                                      <select  class="form-select form-select-lg mb-2 form-control " aria-label=".form-select-lg example" id="workerSelect">
+                                      </select>
+
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary" onclick="selectRoomNum2()">Save changes</button>
+                                  <!-- <input type="submit" value="reassign" class="btn btn-primary"  id="Assign">
+                                  <input type="submit" value="later" class="btn btn-primary"  id="Assign"> -->
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
 
           <!-- reassign window -->
-
             <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
@@ -181,16 +217,79 @@
                     </div>
                 </div>
             </div>
-</body>
+          </div>
+<div id="message">
+
+</div>
+<?php
+	$servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password,"muqu");
+
+    if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+	foreach($_POST as $key => $value) {
+		if (strpos($key, 'box_') === 0) {
+			$complainID = str_replace("box_", '', $key) ;
+			$pickerName = "SelectPicker_".$complainID;
+
+
+				 '<br/>'.$updateSql = "UPDATE complaint
+						SET status = 'solved'
+						WHERE complaintid = $complainID ";
+				if ($conn->query($updateSql) === TRUE) {
+
+				   // else {
+				   //     echo "Error: " . $sql . "<br>" . $conn->error;
+				   //   }
+
+			}
+		}
+	}
+
+
+
+
+?>
+
+<!-- <!doctype html>
+<html lang="en">
+  <head>
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="bootstrap5/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<script src="bootstrap5/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+ <title>Make Complain</title>
+  </head>
+  <body>
+	<header class="navbar navbar-expand-md navbar-dark bd-navbar" > </header>
+
+			<div class="container-fluid">
+				<div class="row justify-content-center ">
+					<div class="card" style="width: 18rem;">
+
+					  <div class="card-body">
+						<h5 class="card-title">Assign Employee </h5>
+						<p class="card-text">Thanks! You confirmed  complaint successfully !</p>
+						<a href="anothercopyassign.php" class="btn btn-primary">Go Back</a>
+					  </div>
+					</div>
+				 </div>
+			</div>
+		</body>
+		</html> -->
+
+          </body>
             <script>
-//             $(document).on("click", "#YourID", function() {
-//               function selectRoomNum(str){
-//
-//
-//
-//               }
-// });
-// ajax for get c
+
+
+             // ajax for get complaint
             function selectRoomNum(strq){
               var str= strq.value;
 
@@ -211,18 +310,97 @@
             xmlhttp.send();
 
             }
-
+////////////////////////////////
+// function getCheckValue{
+//   const ckb = document.querySelectorAll("#checkbox1 input[type=checkbox]");
 //
+// [...ckb].forEach( el => {
+//
+//   if( el.checked ) {
+//     // Is checked!
+//     console.log( el.value )
+//     el.closest("label").style.background = "gold";
+//   } else {
+//     // Not checked one
+//     // ... do something else
+//     el.closest("label").style.background = "gray";
+//   }
+//
+// });
+// }
+
+// const js = document.querySelector('#checkbox1');
+//     const bt = document.querySelector('#bt1');
+//     bt.onclick = () => {
+//        alert(js.name);
+//
+//     };
+
+// choose worker
+function LoadA()
+{
+console.log("hi");
+
+   var xmlhttp = new XMLHttpRequest();
+   xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("workerSelect").innerHTML = this.responseText;
+       }
+   };
+    xmlhttp.open("GET","getRoom.php",true);
+   xmlhttp.send();
+
+}
+            function selectRoomNum2(strq){
+              var allCheckedBox =document.querySelectorAll('input[type="checkbox"]:checked');
+              for (var checkbox of allCheckedBox) { // to check if it select an employee
+                var ourbox = checkbox.id;
+                var workerid=  document.getElementById("workerSelect").value;
+
+                var ourBoxId = ourbox.replace(/\D+/,"");
+                alert(ourBoxId);
+                alert(workerid);
+
+                let part1 = "SelectPicker_";
+                let result = part1.concat(ourBoxId);
+                var ds =document.getElementById(result);
+                var strUser = ds.options[ds.selectedIndex].value;
+
+            var workerid=  document.getElementById("workerSelect").value;
+alert(workerid);
+alert(ourBoxId);
+              var str= strq.value;
+                if (str=="") {
+              document.getElementById("exampleModal").innerHTML="";
+              return;
+            }
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange=function() {
+              if (this.readyState==4 && this.status==200) {
+
+
+                document.getElementById("Ctable").innerHTML=this.responseText;
+
+              };
+            }
+            xmlhttp.open("GET","getComplaint0.php?q="+str,true);
+            xmlhttp.send();
+}
+            }
+// ما يضغط لين سلكت
          			function validateMyForm(event)
                      {
          				var allCheckedBox =document.querySelectorAll('input[type="checkbox"]:checked');
          				for (var checkbox of allCheckedBox) { // to check if it select an employee
          					var ourbox = checkbox.id;
          					var ourBoxId = ourbox.replace(/\D+/,"");
+
          					let part1 = "SelectPicker_";
          					let result = part1.concat(ourBoxId);
          					var ds =document.getElementById(result);
          					var strUser = ds.options[ds.selectedIndex].value;
+                  console.log("hi");
+
 
          					//var strUser1 = e.options[e.selectedIndex].text;
          					if(strUser==0)
@@ -243,56 +421,43 @@
          								// returnToPreviousPage();
          				}
                          return true;
+
                      }
                      // second button
-                     function validateMyForm(event)
-                            {
-                       var allCheckedBox =document.querySelectorAll('input[type="checkbox"]:checked');
-                       for (var checkbox of allCheckedBox) { // to check if it select an employee
-                         var ourbox = checkbox.id;
-                         var ourBoxId = ourbox.replace(/\D+/,"");
-                         let part1 = "SelectPicker_";
-                         let result = part1.concat(ourBoxId);
-                         var ds =document.getElementById(result);
-                         var strUser = ds.options[ds.selectedIndex].value;
+                     // function validateMyForm(event)
+                     //        {
+                     //   var allCheckedBox =document.querySelectorAll('input[type="checkbox"]:checked');
+                     //   for (var checkbox of allCheckedBox) { // to check if it select an employee
+                     //     var ourbox = checkbox.id;
+                     //     var ourBoxId = ourbox.replace(/\D+/,"");
+                     //     let part1 = "SelectPicker_";
+                     //     let result = part1.concat(ourBoxId);
+                     //     var ds =document.getElementById(result);
+                     //     var strUser = ds.options[ds.selectedIndex].value;
+                     //
+                     //
+                     //     //var strUser1 = e.options[e.selectedIndex].text;
+                     //     if(strUser==0)
+                     //     {
+                     //        ds.classList.add("is-invalid");
+                     //        ds.focus();
+                     //
+                     //       event.preventDefault();
+                     //       event.stopPropagation();
+                     //       //window.history.back();
+                     //
+                     //       return false;
+                     //     }else{
+                     //
+                     //       ds.classList.remove("is-invalid");
+                     //
+                     //     }
+                     //
+                     //           // returnToPreviousPage();
+                     //   }
+                     //            return true;
+                     //        }
 
-                         //var strUser1 = e.options[e.selectedIndex].text;
-                         if(strUser==0)
-                         {
-                            ds.classList.add("is-invalid");
-                            ds.focus();
-
-                           event.preventDefault();
-                           event.stopPropagation();
-                           //window.history.back();
-                           return false;
-                         }else{
-
-                           ds.classList.remove("is-invalid");
-
-                         }
-
-                               // returnToPreviousPage();
-                       }
-                                return true;
-                            }
-         /*$(document).ready(function(){
-         	//document.querySelectorAll("[name^=q1_]")
-
-           var myrow1=0;
-           $(".box").each(function() {
-             myrow1=myrow1+1;
-             $(this).attr("id","box_"+ myrow1);
-           });
-          var myrow2=0;
-           $(".form-select").each(function() {
-             myrow2=myrow2+1;
-             $(this).attr("id","SelectPicker_"+ myrow2);
-           });
-
-         });
-
-         */
 
          // search
          function myFunction() {
@@ -365,7 +530,7 @@
 
 
 
-
+//مايشتغل
         // coloring checked row
          function chk(result){
            if(result.checked){
@@ -376,6 +541,8 @@
              result.parentNode.parentNode.style.color="";
            }
          }
+
+         //مايشتغل
         // select all
            function toggle(source) {
             var  checkboxes = document.getElementsByName('foo');
@@ -388,6 +555,7 @@
                callFunction();
              }
            }
+
 
          //  $('input[name="foo"]').on('change', function() {
          //  $(this).closest('tr').toggleClass('yellow', $(this).is(':checked'));
