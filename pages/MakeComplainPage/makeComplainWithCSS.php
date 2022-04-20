960157bacaf035c934a2cc87bbb31184e3378163
RahafMAlharbi <97194765+RahafMAlharbi@users.noreply.github.com>
2022-04-20 17:33:55 +0300

    Update makeComplainWithCSS.php
    Update css

--- pages/MakeComplainPage/makeComplainWithCSS.php
+++ pages/MakeComplainPage/makeComplainWithCSS.php
@@ -71,13 +71,16 @@
     background:#6F92A0
 }
 .card-h{
-  background:#6F92A0
+  background:#2F5972
+
 }
 .Make-Complain{
   color: #edf1f7;
 }
 .card-body{
-  background:#F6F4F1
+  background:#F6F4F1;
+  border:#2F5972;
+border-color:#2F5972;
 
 }
 .input-defult{
@@ -89,7 +92,140 @@ box-sizing: border-box;
 border: 1px solid rgb(212, 212, 212);
 border-radius: 4px;
 }
-input {accent-color:#6F92A0;}
+/* uploas img button */
+.download-button {
+position: relative;
+border-width: 0;
+color: white;
+font-size: 15px;
+font-weight: 600;
+border-radius: 4px;
+z-index: 1;
+}
+
+.download-button .docs {
+display: flex;
+align-items: center;
+justify-content: space-between;
+gap: 10px;
+min-height: 40px;
+padding: 0 10px;
+border-radius: 4px;
+z-index: 1;
+background-color: #242a35;
+border: solid 1px #e8e8e82d;
+transition: all .5s cubic-bezier(0.77, 0, 0.175, 1);
+}
+
+.download-button:hover {
+/* box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px; */
+}
+
+.download {
+position: absolute;
+inset: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+max-width: 90%;
+margin: 0 auto;
+z-index: -1;
+border-radius: 4px;
+transform: translateY(0%);
+background-color: #2F5972;
+border: solid 1px #01e0572d;
+transition: all .5s cubic-bezier(0.77, 0, 0.175, 1);
+}
+
+.download-button:hover .download {
+transform: translateY(100%)
+}
+
+.download svg polyline,.download svg line {
+animation: docs 1s infinite;
+}
+
+@keyframes docs {
+0% {
+transform: translateY(0%);
+}
+
+50% {
+transform: translateY(-15%);
+}
+
+100% {
+transform: translateY(0%);
+}
+}
+
+
+
+/* css QR button */
+.buttonQR {
+  /* width:190px; */
+  color: #edf1f7;
+
+ cursor: pointer;
+ /* font-weight: 700; */
+ font-family: Helvetica,"sans-serif";
+ transition: all .2s;
+ padding: 10px 20px;
+ border-radius: 100px;
+ background: #2F5972;
+ border: 1px solid transparent;
+ display: flex;
+ align-items: center;
+ font-size: 15px;
+}
+
+.buttonQR:hover {
+ background: #6F92A0;
+}
+
+.buttonQR > .svgQR {
+ width: 28px;
+ height: 28px;
+ margin-left: 10px;
+ transition: transform .3s ease-in-out;
+}
+
+.buttonQR:hover .svgQR {
+ transform: translateX(5px);
+}
+
+.buttonQR:active {
+ transform: scale(0.95);
+}
+
+/* submit */
+/* From uiverse.io by @Gumpack */
+.btnSUB {
+ background-image: linear-gradient(45deg, #2F5972 0%, #6F92A0  100%, #2F5972  60%)
+}
+
+.btnSUB {
+  height: 40px;
+
+ margin: 5px;
+ padding: 8px 25px 10px 25px;
+ text-align: center;
+ transition: 0.5s;
+ background-size: 200% auto;
+ color: white;
+ border-radius: 100px;
+ display: block;
+ border: 0px;
+ /* font-weight: 700; */
+ box-shadow: 0px 0px 14px -7px #f09819;
+}
+
+.btnSUB:hover {
+ background-position: right center;
+ /* change the direction of the change here */
+ color: #fff;
+ text-decoration: none;
+}
 
   </style>
 
@@ -190,16 +326,33 @@ input {accent-color:#6F92A0;}
 						  </div>
 
 						<div class="col-12">
-							<div class="mb-3 choose_file">
-                <span class="position-absolute ps-2 pt-1">Choose File</span>
+              <!-- uppload img input  -->
+							<div class="" style="height:0px;overflow:hidden">
+                <span class="position-absolute ps-2 pt-1"></span>
 
 							  <input type="file" class="" id="inputGroupFile02" name="inputGroupFile" accept="image/*">
 							  <!-- <label class="" for="inputGroupFile">Upload</label> -->
 							</div>
+              <!-- uppload img botton -->
+              <button type="button" class=" download-button" onclick="chooseFile();">
+                    <div class="docs">
 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-image" viewBox="0 0 16 16">
+                      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                      <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54A.505.505 0 0 1 1 12.5v-9a.5.5 0 0 1 .5-.5h13z"/>
+                    </svg></div>
+                    <div class="download">
+                      <!-- <svg class="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="currentColor" height="24" width="24" viewBox=""><path d=""></path><polyline points=""></polyline></svg> -->
+
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
+                        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                        <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
+                      </svg>
+                    </div>
+                  </button>
 						</div>
 
-						  <div class="col-12 ">
+						  <div class="col-12 mt-5 ">
 						   <fieldset class="row mb-3 inv" id="content_1">
 								<legend class="col-form-label col-sm-2 pt-0">Category</legend>
 								<div class="col-sm-10">
@@ -264,10 +417,10 @@ input {accent-color:#6F92A0;}
 									  PC
 									</label>
 									</div>
-									 <div class="col-12">
-										<div class="input-group mb-3 ">
-										  <label class=""  id="selectPickerLbl" for="inputGroupSelect01">Computer Number</label>
-										  <select class="form-select  selectpicker" id="mySelectpicker" multiple   name="pcnum"   onchange="changeFun();">
+									 <div class="">
+										<div class="input-group mb-3 row">
+										  <label class="form-label mt-4 mb-3"  id="selectPickerLbl" for="inputGroupSelect01">Computer Number</label>
+										  <select class="form-select me-2 selectpicker" id="mySelectpicker" multiple   name="pcnum"   onchange="changeFun();">
 											<option>pc1</option>
 											<option>pc2</option>
 											<option>pc3</option>
@@ -280,15 +433,26 @@ input {accent-color:#6F92A0;}
                 <input name = "category" value="" type="hidden" id="category">‏
 							 </fieldset>
 							</div>
+               <!-- QR code button -->
               <div class="d-grid gap-2 col-4 mx-auto">
-              <button type="button" class="ms-4 QRcodeB" onclick="myFunction()">Scan QR code   <img src="QRcode.png "height ="30" width="30"/></button>
+              <!-- <button type="button" class="ms-4 QRcodeB" onclick="myFunction()">Scan QR code   <img src="QRcode.png "height ="30" width="30"/></button> -->
+              <button type="button" class="ms-3 buttonQR QRcodeB" onclick="myFunction()">
+                <span>Scan QR code</span>
+
+                  <svg class="svgQR" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-qr-code-scan" viewBox="0 0 16 16">
+                    <path d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1v2.5a.5.5 0 0 1-1 0v-3Zm12 0a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0V1h-2.5a.5.5 0 0 1-.5-.5ZM.5 12a.5.5 0 0 1 .5.5V15h2.5a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5Zm15 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H15v-2.5a.5.5 0 0 1 .5-.5ZM4 4h1v1H4V4Z"/>
+                    <path d="M7 2H2v5h5V2ZM3 3h3v3H3V3Zm2 8H4v1h1v-1Z"/>
+                    <path d="M7 9H2v5h5V9Zm-4 1h3v3H3v-3Zm8-6h1v1h-1V4Z"/>
+                    <path d="M9 2h5v5H9V2Zm1 1v3h3V3h-3ZM8 8v2h1v1H8v1h2v-2h1v2h1v-1h2v-1h-3V8H8Zm2 2H9V9h1v1Zm4 2h-1v1h-2v1h3v-2Zm-4 2v-1H8v1h2Z"/>
+                    <path d="M12 9h2V8h-2v1Z"/>
+                  </svg>
+              </button>
              </div>
 
-
-             <div id="myDIV" style="display:none">
-               <div  class="row">
-                 <div class="col">
-                 <center><div style="width:400px;" id="reader"></div></center>
+             <div id="myDIV" style="display:none" >
+               <div  class="row ">
+                 <div class="col me-5 ">
+                 <center><div style="width:300px;" id="reader"></div></center>
                   </div>
                 </div>
              </div>
@@ -297,7 +461,8 @@ input {accent-color:#6F92A0;}
 <span class="border-top mb-4">
             	<div class="mt-3">
 
-							<button class="choose_file" name="submit" data-bs-toggle="modal" data-bs-target="#exampleModal1" type="submit">Submit form</button>
+							<!-- <button class="choose_file" name="submit" data-bs-toggle="modal" data-bs-target="#exampleModal1" type="submit">Submit form</button> -->
+<button class=" buttonQR mt-3 mb-3 ms-4 me-4 " name="submit" data-bs-toggle="modal" data-bs-target="#exampleModal1" type="submit">Submit</button>
 </span>
               </div>
 						</form>
@@ -309,6 +474,11 @@ input {accent-color:#6F92A0;}
 
 
 <script>
+//jquery for button
+function chooseFile() {
+    $("#inputGroupFile02").click();
+ }
+
       function myFunction() {
               var x = document.getElementById("myDIV");
               if (x.style.display === "none") {
