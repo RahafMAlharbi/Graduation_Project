1955d6a102d545aa39e317ccb4e53ac6f2e35dec
BayaderAlharbi <86174401+BayaderAlharbi@users.noreply.github.com>
2022-04-27 01:34:25 +0300

    Update getComplaints.php

--- LastModify/getComplaints.php
+++ LastModify/getComplaints.php
@@ -2,8 +2,8 @@
 <html>
   <head>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
   </head>
   <body>
 <style media="screen">
@@ -128,7 +128,7 @@ font-size: 20px;
   $ii++;
   if( !empty($row1["images"])){
     ?>
-    <label for="inputPassword6" class="mtop col-form-label">Image :</label>
+   <label for="inputPassword6" class="mtop col-form-label">Image</label>
    <p><img src="<?php echo $row1["images"]; ?>" width="100" height="100" /> </p>
 
   <?php }
