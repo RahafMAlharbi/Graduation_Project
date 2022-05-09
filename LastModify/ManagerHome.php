122921bb63fba94cbab2d8f1617aa516038ea6db
safialfahmi <86435914+safialfahmi@users.noreply.github.com>
2022-05-08 12:16:59 -1200

    Update ManagerHome.php

--- LastModify/ManagerHome.php
+++ LastModify/ManagerHome.php
@@ -320,7 +320,7 @@ ul.nav li a, ul.nav li a:visited {
                                                                                                                                     echo $_SESSION['userName']; ?></p>
                                                                                                                                     </button>
                                                                                                                                       <div class="dropdown-content banner">
-                                                                                                                                      <a href="#">Sign out</a>
+                                                                                                                                      <a href="../logout.php">Sign out</a>
                                                                                                                                       <a href="#">About us</a>
                                                                                                                                       <a href="#">link3</a>
                                                                                                                                       </div>
