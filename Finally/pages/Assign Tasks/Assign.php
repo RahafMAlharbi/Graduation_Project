71103fc2f71ffda6f139df69cc6d92244bf59b28
BayaderAlharbi <86174401+BayaderAlharbi@users.noreply.github.com>
2022-05-17 22:48:14 +0300

    Update Assign.php

--- Finally/pages/Assign Tasks/Assign.php
+++ Finally/pages/Assign Tasks/Assign.php
@@ -11,7 +11,6 @@ session_start();
 <!-- <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet'> -->
 <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet'>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js'></script>
 <script type='text/javascript' src=''></script>
 <title>Assign Tasks</title>
 
@@ -797,6 +796,7 @@ function chk(result){
         <!--end of footer-->
 </footer>
 
+<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js'></script>
 
 
 
