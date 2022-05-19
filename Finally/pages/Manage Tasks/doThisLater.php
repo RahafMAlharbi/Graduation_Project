6be8ae3f36309e7fa981013a0aa3f78f67f6091b
RahafMAlharbi <97194765+RahafMAlharbi@users.noreply.github.com>
2022-05-19 13:17:43 +0300

    Update doThisLater.php

--- Finally/pages/Manage Tasks/doThisLater.php
+++ Finally/pages/Manage Tasks/doThisLater.php
@@ -1,6 +1,6 @@
 <?php
-
-
+  // do this later
+	
 	 $servername = "localhost";
 	 $username = "root";
 	 $password = "";
@@ -11,12 +11,13 @@
 	 if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	 }
+	 // get value from ajax requests in manage pages
 	 $complainId=$_GET['complainId'];
 	 $workerId=$_GET['workerId'];
 	 mysqli_select_db($conn,"muqu");
-	 $sql1  ="DELETE FROM deal WHERE complaintId ='$complainId'" ;
-	 // $sql1  ="DELETE FROM deal WHERE complaintId ='$complainId'" ;
 
+	 // delate complaint from deal table when chose do this later
+	 $sql1  ="DELETE FROM deal WHERE complaintId ='$complainId'" ;
 
 	if ($conn->query($sql1) === TRUE) {
 	  echo "Record deleted successfully 1 ";
@@ -24,26 +25,22 @@
 	  echo "Error deleting record: " . $conn->error;
 	}
 
-	  // $sql2  ="DELETE FROM assign WHERE complaintId ='$complainId' and userId = '$workerId'" ;
-		$sql2  ="DELETE FROM assign WHERE compliantId ='$complainId'" ;
+	// delate complaint from assign table when chose do this later
+	$sql2  ="DELETE FROM assign WHERE compliantId ='$complainId'" ;
 
 	if ($conn->query($sql2) === TRUE) {
 	  echo "Record deleted successfully2 ";
 	} else {
 	  echo "Error deleting record: " . $conn->error;
 	}
+	// set complaint status to Not Processed when press do this later
 	 $sql = "UPDATE complaint SET status='Not Processed' WHERE complaintId='$complainId'" ;
 	if ($conn->query($sql) === TRUE) {
 	  echo "Record deleted successfully";
 	} else {
 	  echo "Error deleting record: " . $conn->error;
 	}
-
 	$conn->close();
 	 $result = mysqli_query($conn,$sql);
-
 	 echo "sucess";
-
-
-
  ?>
