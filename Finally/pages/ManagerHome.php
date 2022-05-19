d29ffea372b2cbc9b96c58eecf622d00ee26f11e
RahafMAlharbi <97194765+RahafMAlharbi@users.noreply.github.com>
2022-05-19 15:09:13 +0300

    Update ManagerHome.php

--- Finally/pages/ManagerHome.php
+++ Finally/pages/ManagerHome.php
@@ -602,7 +602,7 @@ session_start();
 
           <!-- Copyright -->
           <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
+            © 2022 Copyright:
             <a class="text-white" href="#">MUQU.com</a>
           </div>
           <!-- Copyright -->
