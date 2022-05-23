ba3a92901f2c8034b1640728aeafa74dc3c8a655
areej-jameel <85810932+areej-jameel@users.noreply.github.com>
2022-05-23 03:44:34 +0300

    Update Manage.php

--- Finally/pages/Manage Tasks/Manage.php
+++ Finally/pages/Manage Tasks/Manage.php
@@ -35,7 +35,7 @@ $result = $conn->query($qury);
 <!-- jquery Reference-->
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <!-- <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script> -->
-
+<link rel="icon" type="image/x-icon" href="../img/muqu2.png">
 <title>Mange Assign Tasks</title>
 
 </head>
